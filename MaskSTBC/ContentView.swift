820a05884d7b33698d96cda9eb6b859efdfa7dce
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 16:57:58 +0200

    ready

--- MaskSTBC/ContentView.swift
+++ MaskSTBC/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rating = 4
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            starsView
+                .overlay(overlayView.mask(starsView))
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                VStack(spacing: 0) {
+                    Color.blue
+                    Color.yellow
+                }
+                .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+        
     }
 }
 
