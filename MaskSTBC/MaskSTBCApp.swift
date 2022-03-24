802244b2661f02bad4f3d07e8b381f6a568a2889
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 16:09:46 +0200

    Initial Commit

---
+++ MaskSTBC/MaskSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaskSTBCApp.swift
+//  MaskSTBC
+//
+//  Created by Vahtee Boo on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MaskSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
